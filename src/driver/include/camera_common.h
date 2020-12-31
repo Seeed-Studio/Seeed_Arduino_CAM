c7dde8bf9107400498e2fffb126a3123a5e109da
wangcoolc <1387172050@163.com>
2020-12-31 18:14:10 +0800

    Add dma lldesc_t define

--- src/driver/include/camera_common.h
+++ src/driver/include/camera_common.h
@@ -26,6 +26,27 @@ typedef union {
     uint32_t val;
 } dma_elem_t;
 
+
+#define	STAILQ_ENTRY(type)						\
+struct {								\
+	struct type *stqe_next;	/* next element */			\
+}
+
+/* this bitfield is start from the LSB!!! */
+typedef struct lldesc_s {
+    volatile uint32_t size  :12,
+                        length:12,
+                        offset: 5, /* h/w reserved 5bit, s/w use it as offset in buffer */
+                        sosf  : 1, /* start of sub-frame */
+                        eof   : 1, /* end of frame */
+                        owner : 1; /* hw or sw */
+    volatile uint8_t *buf;       /* point to buffer data */
+    union{
+        volatile uint32_t empty;
+        STAILQ_ENTRY(lldesc_s) qe;  /* pointing to the next desc */
+    };
+} lldesc_t;
+
 typedef enum {
     /* camera sends byte sequence: s1, s2, s3, s4, ...
      * fifo receives: 00 s1 00 s2, 00 s2 00 s3, 00 s3 00 s4, ...
