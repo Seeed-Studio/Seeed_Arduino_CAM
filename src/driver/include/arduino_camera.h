c7606492a8b12e6a20c9dafca4f7331cec616251
wangcoolc <1387172050@163.com>
2021-01-05 20:56:05 +0800

    Add definition of error log message

--- src/driver/include/arduino_camera.h
+++ src/driver/include/arduino_camera.h
@@ -75,6 +75,8 @@
 extern "C" {
 #endif
 
+#define CONFIG_OV2640_SUPPORT 1
+
 /**
  * @brief Configuration structure for camera initialization
  */
@@ -120,11 +122,31 @@ typedef struct {
     struct timeval timestamp;   /*!< Timestamp since boot of the first DMA buffer of the frame */
 } camera_fb_t;
 
+
 #define ARDUINO_ERR_CAMERA_BASE 0x20000
-#define ARDUINO_ERR_CAMERA_NOT_DETECTED             (ARDUINO_ERR_CAMERA_BASE + 1)
-#define ARDUINO_ERR_CAMERA_FAILED_TO_SET_FRAME_SIZE (ARDUINO_ERR_CAMERA_BASE + 2)
-#define ARDUINO_ERR_CAMERA_FAILED_TO_SET_OUT_FORMAT (ARDUINO_ERR_CAMERA_BASE + 3)
-#define ARDUINO_ERR_CAMERA_NOT_SUPPORTED            (ARDUINO_ERR_CAMERA_BASE + 4)
+#define ESP_ERR_CAMERA_NOT_DETECTED             (ARDUINO_ERR_CAMERA_BASE + 1)
+#define ESP_ERR_CAMERA_FAILED_TO_SET_FRAME_SIZE (ARDUINO_ERR_CAMERA_BASE + 2)
+#define ESP_ERR_CAMERA_FAILED_TO_SET_OUT_FORMAT (ARDUINO_ERR_CAMERA_BASE + 3)
+#define ESP_ERR_CAMERA_NOT_SUPPORTED            (ARDUINO_ERR_CAMERA_BASE + 4)
+
+typedef int32_t esp_err_t;
+
+/* Definitions for error constants. */
+#define ESP_OK          0       /*!< esp_err_t value indicating success (no error) */
+#define ESP_FAIL        -1      /*!< Generic esp_err_t code indicating failure */
+
+#define ESP_ERR_NO_MEM              0x101   /*!< Out of memory */
+#define ESP_ERR_INVALID_ARG         0x102   /*!< Invalid argument */
+#define ESP_ERR_INVALID_STATE       0x103   /*!< Invalid state */
+#define ESP_ERR_INVALID_SIZE        0x104   /*!< Invalid size */
+#define ESP_ERR_NOT_FOUND           0x105   /*!< Requested resource not found */
+#define ESP_ERR_NOT_SUPPORTED       0x106   /*!< Operation or feature not supported */
+#define ESP_ERR_TIMEOUT             0x107   /*!< Operation timed out */
+#define ESP_ERR_INVALID_RESPONSE    0x108   /*!< Received response was invalid */
+#define ESP_ERR_INVALID_CRC         0x109   /*!< CRC or checksum was invalid */
+#define ESP_ERR_INVALID_VERSION     0x10A   /*!< Version was invalid */
+#define ESP_ERR_INVALID_MAC         0x10B   /*!< MAC address was invalid */
+
 
 /**
  * @brief Initialize the camera driver
